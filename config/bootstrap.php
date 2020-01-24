f310295c46f185340aa3aef6bcffae27f1c8eec3
kicaj <kicaj@kdev.pl>
2020-01-24 23:03:28 +0100

    Renamed configuration keynames

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,10 +6,10 @@ use Cake\Core\Configure;
  */
 Configure::write('Payment', [
     'PayU' => [
-        'url' => env('PAYU_URL'),
-        'pos_id' => env('PAYU_POS_ID'),
-        'signature_key' => env('PAYU_SIGNATURE_KEY'),
-        'client_id' => env('PAYU_CLIENT_ID'),
-        'client_secret' => env('PAYU_CLIENT_SECRET'),
+        'url' => env('PAYMENT_PAYU_URL'),
+        'pos_id' => env('PAYMENT_PAYU_POS_ID'),
+        'signature_key' => env('PAYMENT_PAYU_SIGNATURE_KEY'),
+        'client_id' => env('PAYMENT_PAYU_CLIENT_ID'),
+        'client_secret' => env('PAYMENT_PAYU_CLIENT_SECRET'),
     ],
 ]);
